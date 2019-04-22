e8cca888cb1104015a4ed128326803972eb77256
Cao Liwei <623497281@qq.com>
2019-04-22 14:27:20 +0800

    add 203 solution

--- 203. Remove Linked List Elements/main.cpp
+++ 203. Remove Linked List Elements/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-struct ListNode 
+struct ListNode
 {
 	int val;
 	ListNode *next;
@@ -62,51 +62,52 @@ int printLinkList(ListNode* pHead)
 //虽然也有所谓的pHead，但并不是头结点，而是链表的第一个元素，存储了数据信息。
 //真正头结点的数据域一般不存储任何信息（当然也可以存储如线性表长度等类的附加信息）
 
-//删除顺序链表的相邻重复元素
-ListNode* deleteDuplicates(ListNode* phead)
+//删除顺序链表值为val的元素 
+//我的方案：C++，28 ms， faster than 99.70%，无内存泄露，
+ListNode* removeElements(ListNode* head, int val)
 {
-	if (phead == NULL)
-	{
+	if (head == NULL)
 		return NULL;
-	}
-	if (phead->next == NULL) 
+
+	while (head->val == val) //这一部分参考网友，处理1->1然后删除元素1的链表这种特殊情况
 	{
-		return phead;
+		ListNode *tmp = head;
+		head = head->next;
+		delete tmp;
+		if (head == NULL)
+			return NULL;
 	}
 
-	ListNode* pPre = phead;
-	ListNode* pCur = phead->next;
-	int val = pPre->val;
+	//为了避免内存泄露，链表删除元素需要用到两个指针来记录位置。
+	ListNode* pPre = head;
+	ListNode* pCur = head->next;
 	while (pCur)
-	{		
-		if (pCur->val != val)
+	{
+		if (pCur->val == val)
 		{
-			//如果下一个元素之前元素不等，则存入该值，然后继续后移
-			val = pCur->val;
-			pPre = pCur;
-			pCur = pCur->next;
-			
-			continue;
-		}	 
-		else
-		{
-			//删除结点
+
 			pPre->next = pCur->next;
-			//free(pCur);
 			delete pCur;
 			pCur = pPre->next;
-			
+		}
+		else
+		{
+			pPre = pCur;
+			pCur = pCur->next;
 		}
 	}
-	return phead;
+	return head;
 }
 
 int main()
 {
 	ListNode* SList = createLinkList();
-	//printLinkList(SList);
-	deleteDuplicates(SList);
 	printLinkList(SList);
+	//deleteDuplicates(SList);
+	//printLinkList(SList);
+	printf("after remove element：");
+	ListNode* SListNew = removeElements(SList, 1);
+	printLinkList(SListNew);
 	return 0;
 }
 
